0661823eee4a2587176374048519aa4d3a31e468
ProSavage <naman0311@gmail.com>
2020-08-26 14:14:47 -0500

    website w/ darkmode :D

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,76 @@
 import Head from 'next/head'
+import {useState} from "react";
+import {Sun, Moon} from "react-feather"
 
 export default function Home() {
+
+  const [dark, setDark] = useState(true)
+
   return (
-    <div className="container">
+    <div className={`container ${dark ? "dark" : ""}`} >
       <Head>
-        <title>Create Next App</title>
+        <title>Hey Cutie</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Hi, Megan.
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          I hope you're having a great day.
         </p>
 
+        <div className="toggle-container">
+          {dark && <Sun onClick={() => {
+            setDark(false)
+          }}/>}
+          {!dark && <Moon onClick={() => {
+            setDark(true)
+          }}/>}
+        </div>
+
+        <code className={`${dark ? "dark-code" : ""}`}>Always Remember: You are a cutie.</code>
+
+       
+
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <a href="https://auburn.instructure.com" className="card">
+            <h3>Canvas &rarr;</h3>
+            <p>School is important!</p>
           </a>
 
           <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
+            href="https://auaccess.auburn.edu"
             className="card"
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h3>AUAccess &rarr;</h3>
+            <p>You're amazing.</p>
           </a>
 
           <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="https://outlook.office.com"
             className="card"
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>Outlook &rarr;</h3>
+            <p>Check yo' email.</p>
+          </a>
+
+          <a href="https://amazon.com" className="card">
+            <h3>Amazon &rarr;</h3>
+            <p>Shoppin'.</p>
+          </a>
+
+          <a
+            href="https://www.google.com/search?q=cute+dog+pics&sxsrf=ALeKk02j6LDqo0A0nBh_6_NvSNw6S3Rksg:1598465971025&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiQso3jvbnrAhVwplkKHZ5qAk0Q_AUoAXoECA0QAw&biw=896&bih=984"
+            className="card"
+          >
+            <h3>Cute Dog Pics &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              In case you are feeling sad.
             </p>
           </a>
         </div>
@@ -50,16 +78,25 @@ export default function Home() {
 
       <footer>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://prosavage.net"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+          I love you.
         </a>
       </footer>
 
       <style jsx>{`
+
+        .dark {
+          background: black;
+          color: white;
+        }
+
+        .dark-code {
+          color: black;
+        }
+
         .container {
           min-height: 100vh;
           padding: 0 0.5rem;
@@ -67,6 +104,12 @@ export default function Home() {
           flex-direction: column;
           justify-content: center;
           align-items: center;
+        }
+
+        .toggle-container {
+          padding-bottom: 25px;
+
+          color: p
         }
 
         main {
@@ -85,6 +128,7 @@ export default function Home() {
           display: flex;
           justify-content: center;
           align-items: center;
+          flex-direction: column;
         }
 
         footer img {
